4f05ef4daf1ae56a9b7ab6d7c7a00306444900ca
맥동우 <t6u80o@naver.com>
2025-02-25 18:39:09 +0900

    feat : input, button components

---
+++ components/form-input.tsx
@@ -0,0 +1,30 @@
+import { InputHTMLAttributes } from "react";
+
+interface InputProps {
+  name: string;
+  errors?: string[];
+}
+
+export default function Input({
+  name,
+  errors = [],
+  ...rest
+}: InputProps & InputHTMLAttributes<HTMLInputElement>) {
+  return (
+    <div className="flex flex-col gap-2">
+      <input
+        className="bg-transparent rounded-md w-full h-10 border-none transition
+        focus:outline-none ring-2 ring-neutral-200 focus:ring-4 focus:ring-indigo-500 
+        placeholder:text-neutral-400"
+        name={name}
+        {...rest}
+        // autoComplete="off"
+      />
+      {errors.map((error, index) => (
+        <span key={index} className="text-red-500 font-medium">
+          {error}
+        </span>
+      ))}
+    </div>
+  );
+}
